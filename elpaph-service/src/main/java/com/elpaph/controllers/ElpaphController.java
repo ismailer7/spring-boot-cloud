8fe38dc8854d15389c8f2584983ffa785201708e
Ismail Rzouki <ismailrzouki7@gmail.com>
2024-10-19 11:18:47 +0100

    eureka client

--- elpaph-service/src/main/java/com/elpaph/controllers/ElpaphController.java
+++ elpaph-service/src/main/java/com/elpaph/controllers/ElpaphController.java
@@ -1,7 +1,16 @@
 package com.elpaph.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1")
+@RestController
 public class ElpaphController {
 
+    @GetMapping("/elpaph")
+    String elpaph() {
+        return "Hello From The Land of Giants.";
+    }
 
 }
